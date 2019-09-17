be73ff30a7bb4367c7f5ec51d39622730da6e073
BlackBracken <me@bracken.black>
2019-09-17 14:59:36 +0900

    add listeners

--- src/main/java/black/bracken/seichienvoy/SeichiEnvoy.kt
+++ src/main/java/black/bracken/seichienvoy/SeichiEnvoy.kt
@@ -1,10 +1,52 @@
 package black.bracken.seichienvoy
 
+import kotlinx.coroutines.runBlocking
+import net.md_5.bungee.api.event.PluginMessageEvent
+import net.md_5.bungee.api.event.ServerConnectEvent
+import net.md_5.bungee.api.plugin.Listener
 import net.md_5.bungee.api.plugin.Plugin
+import net.md_5.bungee.event.EventHandler
+import java.io.ByteArrayInputStream
+import java.io.DataInputStream
 
-class SeichiEnvoy : Plugin() {
+class SeichiEnvoy : Plugin(), Listener {
+
+    companion object {
+        const val CHANNEL = "SeichiAssistBungee"
+        private const val SUB_CHANNEL_RECEIVE = "PlayerDataSavedAndDiscarded"
+    }
 
     override fun onEnable() {
+        with(proxy) {
+            registerChannel(CHANNEL)
+            pluginManager.registerListener(this@SeichiEnvoy, this@SeichiEnvoy)
+        }
+    }
+
+    @EventHandler
+    fun onServerConnect(event: ServerConnectEvent) = runBlocking {
+        val player = event.player
+        val from = player.server.info
+        val to = event.target
+
+        from.sendData(to.name, player.name.toByteArray())
+        // blocking
+    }
+
+    @EventHandler
+    fun onPluginMessage(event: PluginMessageEvent) {
+        if (event.tag != SUB_CHANNEL_RECEIVE) return
+
+        val input = DataInputStream(ByteArrayInputStream(event.data))
+        try {
+            if (input.readUTF() != SUB_CHANNEL_RECEIVE) {
+                return
+            }
+
+            // onReceive(input.readUTF())
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 
 }
